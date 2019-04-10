2c9d9fd9ca587883f1db094970725f9f4bc6d9ba
Rachel Wyatt <rw126@evansville.edu>
2019-04-10 19:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function findMatching(drivers, string) {
   findByName = drivers.filter(function (name) { return name == string || name == string.toLowerCase(); });
   return findByName
 }
+
+function fuzzyMatch(drivers, string) {
+    findByFirstInitial = drivers.filter(function (name) { return name == string; });
+  return findByFirstInitial
+}
